c0481fb381d661dbf1765af27308deb21cb35b82
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-07-10 21:45:48 +0800

    123. Interacting with DOM Elements (8min) 

--- kmm/section9/prj-00-initial-starting-setup/src/app.ts
+++ kmm/section9/prj-00-initial-starting-setup/src/app.ts
@@ -1,20 +1,39 @@
 class ProjectInput {
-  temeplateElement: HTMLTemplateElement;
+  templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
-    this.temeplateElement = document.getElementById(
+    this.templateElement = document.getElementById(
       'project-input'
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
     const importedNode = document.importNode(
-      this.temeplateElement.content,
+      this.templateElement.content,
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
@@ -22,4 +41,4 @@ class ProjectInput {
   }
 }
 
-const prjInput = new ProjectInput;
+const prjInput = new ProjectInput();
