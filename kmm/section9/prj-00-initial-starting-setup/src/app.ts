44e7dddaceca17c435a696617ca437547518362c
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-07-10 21:32:03 +0800

    122. DOM Element Selection & OOP Rendering

--- kmm/section9/prj-00-initial-starting-setup/src/app.ts
+++ kmm/section9/prj-00-initial-starting-setup/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class ProjectInput {
+  temeplateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.temeplateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.temeplateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput;
